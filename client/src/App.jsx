fa3e3b446c3217ef6bbb1276497526cbc41263b4
BazamaboiUlt <150413535+BazamaboiUlt@users.noreply.github.com>
2025-09-10 14:29:07 +0800

    Initial project structure for web-based games platform

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Web Games Platform</h1>
+      <p>Welcome! Browse and play hundreds of games.</p>
+    </div>
+  )
+}
+
+export default App
